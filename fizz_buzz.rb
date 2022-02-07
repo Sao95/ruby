75e045e10b69370ffdf31dd0490d9c61712aa545
Sao95 <merasaori@gmail.com>
2022-02-07 12:53:30 +0000

    復習

--- fizz_buzz.rb
+++ fizz_buzz.rb
@@ -1,4 +1,5 @@
 def fizz_buzz(number)
+   # %：割り算の余り
   if number % 15 ==0
     "FizzBuzz"
   elsif number % 3 ==0
@@ -6,13 +7,14 @@ def fizz_buzz(number)
   elsif  number % 5 ==0
     "Buzz"
   else
+    # to_s：整数型→文字列型へ変換
     number.to_s
   end
-    
+
 end
 
 puts "数字を入力してください"
-
+# to_i：文字列型→整数型へ変換
 input = gets.to_i
 
 puts "結果は．．．"
--- lesson4.rb
+++ lesson4.rb
@@ -1,2 +1,2 @@
 name = "Sao"
-puts name
+puts namegit 
